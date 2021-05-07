8494bc7283684bf4b06e77eb218d998deaafdea3
白褶 <919041098@qq.com>
2021-05-07 13:55:19 +0800

    完成英译中/中译英

--- src/main.ts
+++ src/main.ts
@@ -4,25 +4,38 @@ import md5 = require('md5');
 import {appId, appSecret} from './private';
 
 const errorMap = {
-  52001: '请求超时',
-  52002: '系统错误',
-  52003: '未授权用户',
-  52004: '必填参数为空',
-  54001: '签名错误',
+  52000:'成功',
+  52001:'请求超时',
+  52002:'系统错误',
+  52003:'未授权用户',
+  54000:'必填参数为空',
+  54001:'签名错误',
+  54003:'访问频率受限',
+  54004:'账户余额不足',
+  54005:'长query请求频繁',
+  58000:'客户端IP非法',
+  58001:'译文语言方向不支持',
+  58002:'服务当前已关闭',
+  90107:'认证未通过或未生效',
   unknown: '服务器繁忙'
 }
 
 export const translate = (word) => {
   const salt = Math.random();
   const sign = md5(appId + word + salt + appSecret);
+  let from,to
+  if(/[a-zA-Z]/.test(word[0])) {
+    //英译中
+    from = 'en';
+    to = 'zh'
 
+  }else {
+    //中译英
+    from = 'zh';
+    to = 'en'
+  }
   const query: string = querystring.stringify({
-    q: word,
-    from: 'en',
-    to: 'zh',
-    appid: appId,
-    salt: salt,
-    sign: sign
+    q: word, from, to, appid:appId, salt, sign
   });
 
   const options = {
