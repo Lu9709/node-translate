758d728463e0a1cf7987139f37b9f2272dd4bacf
白褶 <919041098@qq.com>
2021-05-07 11:35:39 +0800

    Invaild Sign

---
+++ src/cli.ts
@@ -0,0 +1,15 @@
+import * as commander from 'commander';
+import {translate} from './main';
+
+const program = new commander.Command();
+
+program
+  .version('0.0.1') //版本
+  .name('translate') //名称
+  .usage('<English>') //<>必选
+  .arguments('<English>')
+  .action(function (english){
+    console.log(english);
+    translate(english)
+  })
+program.parse(process.argv);// 参数解析
---
+++ src/private.ts
@@ -0,0 +1,2 @@
+export const appId = '20210507000816805'
+export const appSecret= 'vE2MW8XG5h00gzKBAkjo'
