d08c8db6c9439e16f2d7856ffc6d99ffe4af7b65
白褶 <919041098@qq.com>
2021-05-07 14:27:49 +0800

    NPM发布

--- src/main.ts
+++ src/main.ts
@@ -3,39 +3,45 @@ import * as querystring from 'querystring';
 import md5 = require('md5');
 import {appId, appSecret} from './private';
 
-const errorMap = {
-  52000:'成功',
-  52001:'请求超时',
-  52002:'系统错误',
-  52003:'未授权用户',
-  54000:'必填参数为空',
-  54001:'签名错误',
-  54003:'访问频率受限',
-  54004:'账户余额不足',
-  54005:'长query请求频繁',
-  58000:'客户端IP非法',
-  58001:'译文语言方向不支持',
-  58002:'服务当前已关闭',
-  90107:'认证未通过或未生效',
-  unknown: '服务器繁忙'
+
+// 定义ErrorMap类型
+type ErrorMap = {
+  [key: string]: string
 }
 
-export const translate = (word) => {
+const errorMap: ErrorMap = {
+  52000: '成功',
+  52001: '请求超时',
+  52002: '系统错误',
+  52003: '未授权用户',
+  54000: '必填参数为空',
+  54001: '签名错误',
+  54003: '访问频率受限',
+  54004: '账户余额不足',
+  54005: '长query请求频繁',
+  58000: '客户端IP非法',
+  58001: '译文语言方向不支持',
+  58002: '服务当前已关闭',
+  90107: '认证未通过或未生效',
+  unknown: '服务器繁忙'
+};
+
+export const translate = (word: string) => {
   const salt = Math.random();
   const sign = md5(appId + word + salt + appSecret);
-  let from,to
-  if(/[a-zA-Z]/.test(word[0])) {
+  let from, to;
+  if (/[a-zA-Z]/.test(word[0])) {
     //英译中
     from = 'en';
-    to = 'zh'
+    to = 'zh';
 
-  }else {
+  } else {
     //中译英
     from = 'zh';
-    to = 'en'
+    to = 'en';
   }
   const query: string = querystring.stringify({
-    q: word, from, to, appid:appId, salt, sign
+    q: word, from, to, appid: appId, salt, sign
   });
 
   const options = {
@@ -46,7 +52,7 @@ export const translate = (word) => {
   };
 
   const request = https.request(options, (response) => {
-    let chunks = [];
+    let chunks: Buffer[] = [];// chunks是个buffer数组
     response.on('data', (chunk) => {
       chunks.push(chunk);
     });
